7cb5816f8b2ab47e93a6075885c8f592c20ced78
“ramandeep2008singh@gmail.com” <ramandeep2008singh@gmail.com>
2019-03-25 01:27:09 +0530

    hello

---
+++ MyFramework/src/main/java/com/practice/myjavaprograms/ArrayListInsideArrayList.java
@@ -0,0 +1,9 @@
+package com.practice.myjavaprograms;
+
+public class ArrayListInsideArrayList {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ MyFramework/src/main/java/com/practice/myjavaprograms/ReflectionApiExample.java
@@ -0,0 +1,10 @@
+package com.practice.myjavaprograms;
+
+public class ReflectionApiExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
